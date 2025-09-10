dfa6b8e5e6027d9b0306ba4a80accbf0225eae16
simojibou <34414123+simojibou@users.noreply.github.com>
2025-09-10 23:04:25 +0200

    Create server.js

---
+++ server.js
@@ -0,0 +1,20 @@
+// server.js
+const express = require('express');
+const fetch = require('node-fetch');
+const app = express();
+
+app.get('/affiliate-data', async (req, res) => {
+  const apiKey = 'YOUR_API_KEY';
+  const apiSecret = 'YOUR_API_SECRET';
+  const timestamp = Date.now();
+  const queryString = `api_key=${apiKey}&timestamp=${timestamp}`;
+  const crypto = require('crypto');
+  const sign = crypto.createHmac('sha256', apiSecret).update(queryString).digest('hex');
+  const url = `https://api.bybit.com/v5/affiliate/aff-user-list?${queryString}&sign=${sign}`;
+
+  const response = await fetch(url);
+  const data = await response.json();
+  res.json(data);
+});
+
+app.listen(3000, () => console.log('Server running on port 3000'));
