7f9cfb76713b59be6e24acf1d3d9c319059810f0
simojibou <34414123+simojibou@users.noreply.github.com>
2026-01-19 20:02:32 +0100

    Update OneSignalSDKWorker.js

--- OneSignalSDKWorker.js
+++ OneSignalSDKWorker.js
@@ -1,9 +1,1 @@
 importScripts("https://cdn.onesignal.com/sdks/web/v16/OneSignalSDK.sw.js");
-
-self.addEventListener('install', function(event) {
-    self.skipWaiting(); // إجبار العامل على العمل فوراً
-});
-
-self.addEventListener('activate', function(event) {
-    event.waitUntil(clients.claim()); // السيطرة على الصفحة فوراً لإلغاء خطأ postMessage
-});
